3780e9cada7625b7edbdeac0f5f29af483ed3f4b
leontoys <svliyon@gmail.com>
2025-03-12 09:24:09 +1100

    app-done-render-box-added-styles

--- index.js
+++ index.js
@@ -43,11 +43,65 @@ fileUpload.addEventListener("change",(event)=>{
         imageContainer.appendChild(image)
 
         //TODO-detect image
+        detect(image)
     }
-
 
     //read file
     reader.readAsDataURL(file)
 
 })
+
+const detect = async (image) => {
+    status.textContent = 'Analysing...'
+    //call detector
+    const output = await detector(image.src,{
+        threshold : 0.5,//confidence level , the lower the level more objects it will 
+        //identify, but can go wrong, the higher the level it will identify less but more accurate
+        percentage : true //give the bounding box cordinates as percentages - so that
+        //we can frame the objects
+    })
+    status.textContent = ''
+    //this will be an array of objects
+    //this will have bounding box coordinates
+    console.log("output",output)
+
+    output.forEach(element => {
+        renderBox(element)
+    });
+}
+
+//we need to add a box and show label on the objects deted
+const renderBox = ({box,label})=>{
+    //read cordinates
+    const {xmax,xmin,ymax,ymin} = box
+
+    //color for the box - hex value
+    const color = "#" +  Math.floor(Math.random()* 0xffffff).toString(16).padStart(6,0)
+
+    //draw box
+    const boxElement = document.createElement("div")
+    boxElement.className = "bounding-box"
+
+
+    //this function renderbox will be called for each object in the output array
+    //so for each object add this box
+    Object.assign(boxElement.style, {
+        borderColor : color,
+        left : 100*xmin + "%",
+        top : 100*ymin + "%",
+        width : 100*(xmax-xmin)+"%",
+        height : 100*(ymax-ymin)+"%"
+    })
+
+    //draw label
+    const labelElement = document.createElement("span")
+    labelElement.textContent = label
+    labelElement.className = "bounding-box-label"
+    labelElement.style.backgroundColor = color
+
+    //add label to box
+    boxElement.appendChild(labelElement)
+    //add box
+    imageContainer.appendChild(boxElement)
+}
 
